aab5ae1963813445e39bf7556539b0ac6ca7ff28
Gabriele Russo Russo <gabri.russo17@gmail.com>
2022-01-13 11:38:29 +0100

    Adds support for single files in the backup list

--- test/test_backup.sh
+++ test/test_backup.sh
@@ -4,6 +4,7 @@ TEST_PREFIX="/tmp/easybackuptest"
 DIR1="$TEST_PREFIX-dir1"
 DIR2="$TEST_PREFIX-dir2"
 DIR3="$TEST_PREFIX-dir3"
+singleFile="$TEST_PREFIX-singlefile"
 LIST_FILE="$TEST_PREFIX-list"
 EXCLUDED_PATTERNS_FILE="$TEST_PREFIX-excluded"
 BACKUP_DIR="$TEST_PREFIX-backup"
@@ -16,9 +17,11 @@ prepare_test()
 	mkdir -p "$DIR1/subdir" 2> /dev/null
 	mkdir "$DIR2" 2> /dev/null
 	mkdir -p "$DIR3/excluded" 2> /dev/null
+	touch $singleFile
 
 	# Creates some files in these directories
 	dd if=/dev/random bs=1500 count=5 iflag=fullblock > $DIR1/file1 2> /dev/null
+	dd if=/dev/random bs=1500 count=5 iflag=fullblock > $singleFile 2> /dev/null
 	dd if=/dev/random bs=1500 count=5 iflag=fullblock > $DIR1/subdir/file2 2>/dev/null
 	dd if=/dev/random bs=1500 count=5 iflag=fullblock > $DIR2/file3 2> /dev/null
 	dd if=/dev/random bs=1500 count=5 iflag=fullblock > $DIR3/file4 2> /dev/null
@@ -28,6 +31,7 @@ prepare_test()
 	echo $DIR1 > $LIST_FILE
 	echo $DIR2 >> $LIST_FILE
 	echo $DIR3 >> $LIST_FILE
+	echo $singleFile >> $LIST_FILE
 
 	# Creates a file with excluded patterns
 	echo '*/excluded/*' > $EXCLUDED_PATTERNS_FILE
@@ -38,6 +42,7 @@ cleanup()
 	rm -rf $DIR1
 	rm -rf $DIR2
 	rm -rf $DIR3
+	rm -rf $singleFile
 	rm -f $LIST_FILE
 	rm -f $EXCLUDED_PATTERNS_FILE
 	rm -rf $BACKUP_DIR
@@ -62,6 +67,13 @@ verify()
 		echo "TEST FAILED" >&2
 		return 1
 	fi
+
+	diff $singleFile $BACKUP_DIR$singleFile
+	if [[ $? -ne 0 ]]; then
+		echo "TEST FAILED" >&2
+		return 1
+	fi
+
 
 	echo "Verifying excluded patterns in DIR3..."
 	if [[ -f $BACKUP_DIR$DIR3/excluded/file5 ]]; then
